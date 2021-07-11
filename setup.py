0b36b7d6f58f82858540510b589708c3534f5b23
Cash Costello <cash.costello@jhuapl.edu>
2021-07-11 08:11:36 -0400

    changed the nltk version requirement

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(name='parsivar',
-	  packages=['parsivar'],
+	  packages=['parsivar-scale'],
 	  version='0.2.3',
 	  description='Python library for preprocessing Persian text.',
 	  author='ICT',
 	  author_email='',
 	  url='',
 	  download_url = '',
-	  install_requires=['nltk==3.4.5'],
+	  install_requires=['nltk>=3.4.5'],
 	  python_requires=">=3.3",
 	  classifiers = [],
 	  include_package_data=True
